72852245075831505d3eaea7fb1f4edab27d9240
Dmitry Romanenko <Dmitry@Romanenko.in>
2020-01-07 00:10:57 -0500

    Start rework on Data/LogCapture

--- WFInfoCS/Data.cs
+++ WFInfoCS/Data.cs
@@ -1,10 +1,14 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using NLua;
 
 namespace WFInfoCS
 {
@@ -18,14 +22,130 @@ namespace WFInfoCS
         public JObject eqmt_data; // Contains eqmt_data from Warframe PC Drops          {<EQMT>: {"vaulted": true, "PARTS": {<NAME>:{"relic_name":<name>|"","count":<num>}, ...}},  ...}
         public JObject name_data; //C ontains relic to market name translation          {<relic_name>: <market_name>}
 
-        private string MarketItemsPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\WFInfoCS\market_items.json";
-        private string MarketDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\WFInfoCS\market_data.json";
-        private string EqpmtDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\WFInfoCS\eqmt_data.json";
-        private string RelicDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\WFInfoCS\relic_data.json";
-        private string NameDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\WFInfoCS\name_data.json";
+        private string ApplicationDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\WFInfoCS";
+        private string _marketItemsPath;
+        private string _marketDataPath;
+        private string _eqpmtDataPath;
+        private string _relicDataPath;
+        private string _nameDataPath;
 
-        WebClient WebClient = new WebClient();
-        private Sheets sheetsAPI;
-        //private NLua.Lua Lua;
+        private int save_count = 0;
+        WebClient WebClient;
+        private FileSystemWatcher _screenshotWatcher = new FileSystemWatcher();
+        private LogCapture _EElogWatcher;
+
+        private Sheets _sheetsApi;
+        private NLua.Lua _lua;
+
+        public Data()
+        {
+            Main.AddLog("CREATING DATABASE");
+            _marketItemsPath = ApplicationDirectory + @"\market_items.json";
+            _marketDataPath = ApplicationDirectory + @"\market_data.json";
+            _eqpmtDataPath = ApplicationDirectory + @"\eqmt_data.json";
+            _relicDataPath = ApplicationDirectory + @"\relic_data.json";
+            _nameDataPath = ApplicationDirectory + @"\name_data.json";
+
+            Directory.CreateDirectory(ApplicationDirectory);
+
+            WebClient = new WebClient();
+            WebClient.Headers.Add("platform", "pc");
+            WebClient.Headers.Add("language", "en");
+
+            _sheetsApi = new Sheets();
+
+            string warframePictures = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + @"\Warframe"; 
+            Directory.CreateDirectory(warframePictures);
+            _screenshotWatcher.Path = warframePictures;
+            _screenshotWatcher.EnableRaisingEvents = true;
+
+            _lua = new NLua.Lua();
+            if (My.Settings.Auto) // WIP
+            {
+                Enable_LogCapture();
+            }
+        }
+
+        private void Enable_LogCapture()
+        {
+            if (_EElogWatcher == null)
+            {
+                try
+                {
+                    _EElogWatcher = new LogCapture();
+                    _EElogWatcher.TextChanged += log_Changed;
+                }
+                catch (Exception ex)
+                {
+                    Main.AddLog("FAILED TO START LogCapture");
+                    // WIP - show on screen on main window that there's problem with log capture
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+
+        private void Disable_LogCapture()
+        {
+            if (_EElogWatcher != null)
+            {
+                _EElogWatcher.TextChanged -= log_Changed;
+                _EElogWatcher.Dispose();
+                _EElogWatcher = null;
+            }
+        }
+
+        private void Save_JObject(JObject data)
+        {
+            Main.AddLog("SAVING DEBUG JSON: debug" + save_count.ToString() + ".json");
+            File.WriteAllText(Path.Combine(ApplicationDirectory + @"\debug" + save_count.ToString() + ".json"), JsonConvert.SerializeObject(data, Formatting.Indented));
+            save_count += 1;
+        }
+
+        private void Save_JArray(JArray data)
+        {
+            Main.AddLog("SAVING DEBUG JSON: debug" + save_count.ToString() + ".json");
+            File.WriteAllText(Path.Combine(ApplicationDirectory + @"\debug" + save_count.ToString() + ".json"), JsonConvert.SerializeObject(data, Formatting.Indented));
+            save_count += 1;
+        }
+
+        private void Save_Market()
+        {
+            Main.AddLog("SAVING MARKET DATABASE");
+            File.WriteAllText(_marketItemsPath, JsonConvert.SerializeObject(market_items, Formatting.Indented));
+            File.WriteAllText(_marketDataPath, JsonConvert.SerializeObject(market_data, Formatting.Indented));
+        }
+
+        private void Save_Relics()
+        {
+            Main.AddLog("SAVING RELIC DATABASE");
+            File.WriteAllText(_relicDataPath, JsonConvert.SerializeObject(relic_data, Formatting.Indented));
+        }
+
+        private void Save_Names()
+        {
+            Main.AddLog("SAVING NAME DATABASE");
+            File.WriteAllText(_nameDataPath, JsonConvert.SerializeObject(name_data, Formatting.Indented));
+        }
+
+        private void Save_Eqmt()
+        {
+            Main.AddLog("SAVING EQMT DATABASE");
+            File.WriteAllText(_eqpmtDataPath, JsonConvert.SerializeObject(eqmt_data, Formatting.Indented));
+        }
+
+        ///
+        ///
+        /// WIP - the rest code is TBD
+        ///
+        /// 
+
+        private void log_Changed(object sender, string line)
+        {
+            Console.WriteLine(line);
+            if (line.Contains("Sys [Info]: Created /Lotus/Interface/ProjectionsCountdown.swf"))
+            {
+                Task.Factory.StartNew(Main.DoDelayWork()); // WIP
+            }
+        }
     }
 }
---
+++ WFInfoCS/LogCapture.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WFInfoCS
+{
+    public delegate void LogWatcherEventHandler(object sender, String text);
+
+    class LogCapture : IDisposable
+    {
+        private MemoryMappedFile memoryMappedFile;
+        private EventWaitHandle bufferReadyEvent;
+        private EventWaitHandle dataReadyEvent;
+        CancellationTokenSource tokenSource = new CancellationTokenSource();
+        private CancellationToken token;
+
+        public event LogWatcherEventHandler TextChanged;
+
+        public LogCapture()
+        {
+            token = tokenSource.Token;
+            Main.AddLog("STARTING LogCapture");
+            memoryMappedFile = MemoryMappedFile.CreateOrOpen("DBWIN_BUFFER", 4096L);
+
+            bufferReadyEvent = new EventWaitHandle(false, EventResetMode.AutoReset, "DBWIN_BUFFER_READY", out Boolean createdBuffer);
+
+            if (!createdBuffer)
+            {
+                Main.AddLog("The DBWIN_BUFFER_READY event exists.");
+                return;
+            }
+
+            dataReadyEvent = new EventWaitHandle(false, EventResetMode.AutoReset, "DBWIN_DATA_READY", out Boolean createdData);
+
+            if (!createdData)
+            {
+                Main.AddLog("The DBWIN_DATA_READY event exists.");
+                return;
+            }
+
+            Task.Factory.StartNew(Run);
+        }
+
+        private void Run()
+        {
+            Process proc = parser2.GetWFProc(); // WIP
+
+            try
+            {
+                TimeSpan Timeout = TimeSpan.FromSeconds(1.0);
+                bufferReadyEvent.Set();
+                while (!token.IsCancellationRequested)
+                {
+                    if (!dataReadyEvent.WaitOne(Timeout))
+                    {
+                        continue;
+                    }
+
+                    if ((proc == null) || (proc.HasExited))
+                    {
+                        proc = parser2.GetWFProc();
+                    }
+
+                    if (proc != null)
+                    {
+                        using (MemoryMappedViewStream stream = memoryMappedFile.CreateViewStream())
+                        {
+                            using (BinaryReader reader = new BinaryReader(stream, Encoding.Default))
+                            {
+                                uint processId = reader.ReadUInt32();
+                                if (processId == proc.Id)
+                                {
+                                    var chars = reader.ReadChars(4092);
+                                    var index = Array.IndexOf(chars, "\0");
+                                    var message = new String(chars, 0, index);
+                                    TextChanged(this, message.Trim());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                if (memoryMappedFile != null)
+                {
+                    memoryMappedFile.Dispose();
+                }
+
+                if (bufferReadyEvent != null)
+                {
+                    bufferReadyEvent.Dispose();
+                }
+
+                if (dataReadyEvent != null)
+                {
+                    dataReadyEvent.Dispose();
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            tokenSource.Cancel();
+            tokenSource.Dispose();
+            Main.AddLog("STOPPING LogCapture");
+        }
+    }
+}
