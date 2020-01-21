2b2e94019e00d8b7791b0829f6c9c52d31e8ccd7
dapal-003 <Dapal003@gmail.com>
2020-01-21 01:43:46 +0100

    Implemented reward detection
    Might not be accurate, needs testing

--- WFInfoCS/Ocr.cs
+++ WFInfoCS/Ocr.cs
@@ -16,47 +16,87 @@ namespace WFInfoCS {
 		}
 		public static HandleRef HandleRef { get; private set; }
 		public static float dpi { get; set; }
-		private static Process Warframe;
-		private static System.Drawing.Point center;
+		private static Process Warframe = null;
+		private static Point center;
 		public static Rectangle window { get; set; }
 		private HandleRef handelRef;
-
+		private static double widthPrecentFirstReward = 0.301953125; // 773 /2560 = 0.301953125
+		private static double widthPrecentSecondReward = 0.428125; // 1096 /2560 =0.428125
+		private static double HeightPrecentReward = 0.430555555556; // 620 / 1440 = 0.430555555556
+		private static Rectangle firstRewardRectangle; //40 x 35 for rare, catches common
+		private static Rectangle secondRewardRectangle;
 		//todo  implemenet Tesseract
 		//      implemenet pre-prossesing
 
 		internal static int findRewards(Bitmap image) {
-			
+			Console.WriteLine();
 			// Firstly check at the first possible possition with 4 rewards, which is at Width = 0.3097 % and Height = 0.4437 %
 			// If not found, check first possible possition with 2 rewards, which is 0.4218 %
 			// If also not found, there are 3 rewards
-			double widthPrecentFirstReward = 0.3097;
-			double widthPrecentSecondReward = 0.4218;
-			double HeightPrecentReward = 0.4437;
-			Bitmap testImage = new Bitmap(7, 7);
-			testImage = image.Clone(new Rectangle((int)(image.Width * widthPrecentFirstReward), (int)(image.Height * HeightPrecentReward), 7, 7), image.PixelFormat);
-			testImage.Save(Main.appPath + @"\Debug\testImage.png");
+			firstRewardRectangle = new Rectangle((int)(image.Width * widthPrecentFirstReward), (int)(image.Height * HeightPrecentReward), 40, 35);
+			secondRewardRectangle = new Rectangle((int)(image.Width * widthPrecentSecondReward), (int)(image.Height * HeightPrecentReward), 40, 35);
 
-			return 0;
-			//throw new NotImplementedException();
+			Bitmap firstReward = new Bitmap(7, 7);
+			Bitmap secondReward = new Bitmap(7, 7);
+			firstReward = image.Clone(firstRewardRectangle, image.PixelFormat);
+			secondReward = image.Clone(secondRewardRectangle, image.PixelFormat);
+			firstReward.Save(Main.appPath + @"\Debug\testImage1.png");
+			secondReward.Save(Main.appPath + @"\Debug\testImage2.png");
+			doDebugDrawing(image);
+			if (verrifyReward(firstReward)) { // 4 rewards
+				Console.WriteLine("4 rewards");
+				return 4;
+			} else if (verrifyReward(secondReward)) { // 2 rewards
+				Console.WriteLine("2 rewards");
+				return 2;
+			} else { // 3 rewards
+				Console.WriteLine("3 rewards");
+				return 3;
+			}
 		}
+
+		private static Boolean verrifyReward(Bitmap image) {
+			for (int x = 0; x < image.Width; x++) {
+				for (int y = 0; y < image.Height; y++) {
+					for (int color = 0; color < 2; color++) {
+						if (ColorThreshold(image.GetPixel(x, y), ColorTranslator.FromHtml("#" + Settings.colorArray["rarityColor"][color].ToString()))) {
+							Main.AddLog("Found color: " + ColorTranslator.FromHtml("#" + Settings.colorArray["rarityColor"][color].ToString()));
+							return true;
+						}
+
+					}
+				}
+			}
+			return false;
+		}
+
 		public static Boolean ColorThreshold(Color test, Color thresh, int Treshold = 10) {
-			return (Math.Abs((int)(test.R) - thresh.R) < Treshold && Math.Abs((int)(test.G) - thresh.G) < Treshold && Math.Abs((int)(test.B) - thresh.B) < Treshold);
+			return (Math.Abs(test.R - thresh.R) < Treshold && Math.Abs(test.G - thresh.G) < Treshold && Math.Abs(test.B - thresh.B) < Treshold);
 		}
 
 		private static void doDebugDrawing(Bitmap image) {
 			using (Graphics graphics = Graphics.FromImage(image)) {
-				graphics.DrawRectangle(new Pen(Color.Red), window);
+				graphics.DrawRectangle(new Pen(Color.Red), new Rectangle(new Point(window.X, window.Y), new Size(window.Width - 1, window.Height - 1)));
+				graphics.DrawRectangle(new Pen(Color.Red), firstRewardRectangle);
+				graphics.DrawRectangle(new Pen(Color.Red), secondRewardRectangle);
 				graphics.FillRectangle(Brushes.Pink, center.X, center.Y, 5, 5);
-				image.Save( Main.appPath + @"\Debug\FullScreenShotDebug.png");
+				image.Save(Main.appPath + @"\Debug\FullScreenShotDebug.png");
 			}
 		}
 
 		public static Boolean verifyFocus() { // Returns True if warframe is in focuse, False if not
 			uint processID = 0;
 			uint threadID = Win32.GetWindowThreadProcessId(Win32.GetForegroundWindow(), out processID);
-			if (processID == Warframe.Id) { return true; } else {
-				Main.AddLog("Warframe is not focused");
-				Main.updatedStatus("Warframe out of focus", 2);
+			try {
+				if (processID == Warframe.Id || Settings.debug) { return true; } else {
+					Main.AddLog("Warframe is not focused");
+					Main.updatedStatus("Warframe is out of focus", 2);
+					return false;
+				}
+			}
+			catch (Exception ex) {
+				Console.WriteLine(Warframe.ToString());
+				Console.WriteLine(ex.ToString());
 				return false;
 			}
 		}
@@ -70,11 +110,10 @@ namespace WFInfoCS {
 					return true;
 				}
 			}
-			if (Settings.debug) { return true; } else {
-				Main.AddLog("Unable to detect Warframe in list of current active processes");
-				Main.updatedStatus("Unable to detect Warframe process", 1);
-				return false;
-			}
+			Main.AddLog("Unable to detect Warframe in list of current active processes");
+			Main.updatedStatus("Unable to detect Warframe process", 1);
+			return false;
+
 		}
 
 		private static Bitmap turnBW(Bitmap source) {
@@ -96,7 +135,7 @@ namespace WFInfoCS {
 			if (!Win32.GetWindowRect(HandleRef, out osRect)) { // get window size of warframe
 				if (Settings.debug && Warframe == null) { //if debug is on AND warframe is not detected, sillently ignore missing process and use main monitor center.
 					Main.AddLog("No warframe detected, thus using center of image");
-					window = new Rectangle(0,0,image.Width,image.Height);
+					window = new Rectangle(0, 0, image.Width, image.Height);
 					center = new Point(window.Width / 2, window.Height / 2);
 					return;
 				} else {
@@ -109,8 +148,8 @@ namespace WFInfoCS {
 			// if the window is in the VOID delete current process and re-set window to nothing
 
 			if (window.Left != osRect.Left || window.Right != osRect.Right || window.Top != osRect.Top || window.Bottom != osRect.Bottom) { // checks if old window size is the right size if not change it
-				window = new Rectangle (osRect.Left, osRect.Top, osRect.Right - osRect.Left, osRect.Bottom - osRect.Top); // gett Rectangle out of rect
-				//Rectangle is (x, y, width, height) RECT is (x, y, x+width, y+height) 
+				window = new Rectangle(osRect.Left, osRect.Top, osRect.Right - osRect.Left, osRect.Bottom - osRect.Top); // gett Rectangle out of rect
+																														 //Rectangle is (x, y, width, height) RECT is (x, y, x+width, y+height) 
 				Main.AddLog("Window size updated to: " + window.ToString());
 				int GWL_style = -16;
 				uint Fullscreen = 885981184;
