728b9e5a6907861b1836b1d29a17ace25b6c9a84
dapal-003 <Dapal003@gmail.com>
2020-02-05 20:22:57 +0100

    Implemented Ui for relics and quipment

--- WFInfoCS/RelicsWindow.cs
+++ WFInfoCS/RelicsWindow.cs
@@ -1,19 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace WFInfoCS
-{
+namespace WFInfoCS {
     /// <summary>
     /// Interaction logic for RelicsWindow.xaml
     /// </summary>
@@ -40,6 +28,10 @@ namespace WFInfoCS
         {
             if (e.ChangedButton == MouseButton.Left)
                 DragMove();
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e) {
+
         }
     }
 }
