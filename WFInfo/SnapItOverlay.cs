0238621b5a519e8d7d3593509f20c17003e72844
dapal-003 <Dapal003@gmail.com>
2020-03-04 15:51:09 +0100

    Removed early disposal of temp image

--- WFInfo/SnapItOverlay.cs
+++ WFInfo/SnapItOverlay.cs
@@ -67,10 +67,11 @@ namespace WFInfo
             }
             tempImage = tempImage.Clone(new Rectangle((int)startDrag.X, (int)startDrag.Y, (int)rectangle.Width, (int)rectangle.Height), System.Drawing.Imaging.PixelFormat.DontCare);
             Task.Factory.StartNew(() => OCR.ProcessSnapIt(tempImage));
-            tempImage.Dispose();
             Topmost = false;
             Hide();
-        }
+            Task.WaitAll();
+            tempImage.Dispose();
+            }
 
         private void canvas_MouseMove(object sender, MouseEventArgs e)
         {
