397a2d05f6d1b7c9a2c57905897da89ff8da5d5d
random-facades <random.facades@gmail.com>
2020-03-11 20:15:01 -0600

    Various Minor Changes Hid rectangle when snapit window closes Made window transparent Moved Text to bottom middle Made scaleup return the original image if it's already taller than 100 pixels Made the overlay appear above the captured region

--- WFInfo/SnapItOverlay.xaml.cs
+++ WFInfo/SnapItOverlay.xaml.cs
@@ -6,30 +6,34 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 
-namespace WFInfo {
+namespace WFInfo
+{
     /// <summary>
     /// Interaction logic for SnapItOverlay.xaml
     /// Marching ant logic by: https://www.codeproject.com/Articles/27816/Marching-Ants-Selection
     /// </summary>
-    public partial class SnapItOverlay : Window {
+    public partial class SnapItOverlay : Window
+    {
         public bool isEnabled;
         public Bitmap tempImage;
         private System.Windows.Point startDrag;
-        public SnapItOverlay() {
+        private System.Drawing.Point topLeft;
+        public SnapItOverlay()
+        {
             InitializeComponent();
-            canvas.MouseDown += new MouseButtonEventHandler(canvas_MouseDown);
-            canvas.MouseUp += new MouseButtonEventHandler(canvas_MouseUp);
-            canvas.MouseMove += new MouseEventHandler(canvas_MouseMove);
+            MouseDown += new MouseButtonEventHandler(canvas_MouseDown);
+            MouseUp += new MouseButtonEventHandler(canvas_MouseUp);
+            MouseMove += new MouseEventHandler(canvas_MouseMove);
         }
 
-        public void Populate(Bitmap screenshot) {
-            var displayImage = Win32.ImageSourceFromBitmap(screenshot);
-            Image.Source = displayImage;
+        public void Populate(Bitmap screenshot)
+        {
             tempImage = screenshot;
             isEnabled = true;
         }
 
-        private void canvas_MouseDown(object sender, MouseButtonEventArgs e) {
+        private void canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
             //Set the start point
             startDrag = e.GetPosition(canvas);
             //Move the selection marquee on top of all other objects in canvas
@@ -40,31 +44,53 @@ namespace WFInfo {
             canvas.Cursor = Cursors.Cross;
         }
 
-        public void closeOverlay() {
+        public void closeOverlay()
+        {
+            rectangle.Width = 0;
+            rectangle.Height = 0;
+            rectangle.RenderTransform = new TranslateTransform(0, 0);
             Topmost = false;
-            Hide();
             isEnabled = false;
+
+            // THIS FUCKING RECTANGLE WOULDN'T GO AWAY 
+            //    AND IT WOULD STAY FOR 1 FRAME WHEN RE-OPENNING THIS WINDOW
+            //    SO I FORCED THAT FRAME TO HAPPEN BEFORE CLOSING
+            //       AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHH
+            //
+            //  fucking hate rectangles
+            Task.Factory.StartNew(async () =>
+            {
+                await Task.Delay(100);
+                Dispatcher.Invoke(Hide);
+            });
         }
 
-        private void canvas_MouseUp(object sender, MouseButtonEventArgs e) {
+        private void canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
             //Release the mouse
             if (canvas.IsMouseCaptured)
                 canvas.ReleaseMouseCapture();
             canvas.Cursor = Cursors.Arrow;
             Main.AddLog("User drew rectangle: Starting point: " + startDrag.ToString() + " Width: " + rectangle.Width + " Height:" + rectangle.Height);
-            if (rectangle.Width < 10 || rectangle.Height < 10) { // box is smaller than 10x10 and thus will never be able to have any text. Also used as a failsave to prevent the program from crashing if the user makes a 0x0 sleection
+            if (rectangle.Width < 10 || rectangle.Height < 10)
+            { // box is smaller than 10x10 and thus will never be able to have any text. Also used as a failsave to prevent the program from crashing if the user makes a 0x0 sleection
                 Main.AddLog("User selected an area too small");
                 Main.StatusUpdate("Please slecet a larger area to scan", 2);
                 return;
             }
-            tempImage = tempImage.Clone(new Rectangle((int)startDrag.X, (int)startDrag.Y, (int)rectangle.Width, (int)rectangle.Height), System.Drawing.Imaging.PixelFormat.DontCare);
-            Task.Factory.StartNew(() => OCR.ProcessSnapIt(tempImage));
-            Topmost = false;
-            Hide();
+
+            Bitmap cutout = tempImage.Clone(new Rectangle(topLeft.X, topLeft.Y, (int)rectangle.Width, (int)rectangle.Height), System.Drawing.Imaging.PixelFormat.DontCare);
+            int xPos = topLeft.X + (int)rectangle.Width / 2;
+            int yPos = topLeft.Y + 10;
+            Task.Factory.StartNew(() => OCR.ProcessSnapIt(cutout,tempImage,xPos,yPos));
+
+            closeOverlay();
         }
 
-        private void canvas_MouseMove(object sender, MouseEventArgs e) {
-            if (canvas.IsMouseCaptured) {
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (canvas.IsMouseCaptured)
+            {
                 System.Windows.Point currentPoint = e.GetPosition(canvas);
 
                 //Calculate the top left corner of the rectangle 
@@ -76,6 +102,7 @@ namespace WFInfo {
                     rectangle.Visibility = Visibility.Visible;
 
                 //Move the rectangle to proper place
+                topLeft = new System.Drawing.Point((int)x, (int)y);
                 rectangle.RenderTransform = new TranslateTransform(x, y);
                 //Set its size
                 rectangle.Width = Math.Abs(e.GetPosition(canvas).X - startDrag.X);
