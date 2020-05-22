7b4c19e8e26ab9826e29276e9b8c39196dcf5f60
dapal-003 <Dapal003@gmail.com>
2020-05-22 22:23:27 +0200

    Change to default settings
    DetectScaling and HighlightRewards are now on by default

--- WFInfo/MainWindow.xaml.cs
+++ WFInfo/MainWindow.xaml.cs
@@ -45,86 +45,7 @@ namespace WFInfo
                     Settings.settingsObj = new JObject();
                     hai = new WelcomeDialogue();
                 }
-                if (!Settings.settingsObj.TryGetValue("Display", out _))
-                    Settings.settingsObj["Display"] = "Overlay";
-                Settings.isOverlaySelected = Settings.settingsObj.GetValue("Display").ToString() == "Overlay";
-                Settings.isLightSlected = Settings.settingsObj.GetValue("Display").ToString() == "Light";
-
-                if (!Settings.settingsObj.TryGetValue("MainWindowLocation_X", out _))
-                    Settings.settingsObj["MainWindowLocation_X"] = 300;
-                if (!Settings.settingsObj.TryGetValue("MainWindowLocation_Y", out _))
-                    Settings.settingsObj["MainWindowLocation_Y"] = 300;
-                Settings.mainWindowLocation = new Point(Settings.settingsObj.GetValue("MainWindowLocation_X").ToObject<Int32>(), Settings.settingsObj.GetValue("MainWindowLocation_Y").ToObject<Int32>());
-
-                if (!Settings.settingsObj.TryGetValue("ActivationKey", out _))
-                    Settings.settingsObj["ActivationKey"] = "Snapshot";
-                try
-                {
-                    Settings.ActivationKey = (Key)Enum.Parse(typeof(Key), Settings.settingsObj.GetValue("ActivationKey").ToString());
-                }
-                catch
-                {
-                    try
-                    {
-                        Settings.ActivationMouseButton = (MouseButton)Enum.Parse(typeof(MouseButton), Settings.settingsObj.GetValue("ActivationKey").ToString());
-                    }
-                    catch
-                    {
-                        Main.AddLog("Couldn't Parse Activation Key -- Defaulting to PrintScreen");
-                        Settings.settingsObj["ActivationKey"] = "Snapshot";
-                        Settings.ActivationKey = Key.Snapshot;
-                    }
-                }
-
-                if (!Settings.settingsObj.TryGetValue("Debug", out _))
-                    Settings.settingsObj["Debug"] = false;
-                Settings.debug = (bool)Settings.settingsObj.GetValue("Debug");
-
-                if (!Settings.settingsObj.TryGetValue("Clipboard", out _))
-                    Settings.settingsObj["Clipboard"] = false;
-                Settings.clipboard = (bool)Settings.settingsObj.GetValue("Clipboard");
-
-                if (!Settings.settingsObj.TryGetValue("Auto", out _))
-                    Settings.settingsObj["Auto"] = false;
-                Settings.auto = (bool)Settings.settingsObj.GetValue("Auto");
-
-                if (!Settings.settingsObj.TryGetValue("CuttingEdge", out _))
-                    Settings.settingsObj["CuttingEdge"] = false;
-                Settings.autoScaling = (bool)Settings.settingsObj.GetValue("CuttingEdge");
-
-                if (!Settings.settingsObj.TryGetValue("AutoDelay", out _))
-                    Settings.settingsObj["AutoDelay"] = 250L;
-                Settings.autoDelay = (long)Settings.settingsObj.GetValue("AutoDelay");
-
-                if (!Settings.settingsObj.TryGetValue("Scaling", out _))
-                    Settings.settingsObj["Scaling"] = 100.0;
-                Settings.scaling = Convert.ToInt32(Settings.settingsObj.GetValue("Scaling"));
-
-                if (!Settings.settingsObj.TryGetValue("ImageRetentionTime", out _))
-                    Settings.settingsObj["ImageRetentionTime"] = 12;
-                Settings.imageRetentionTime = Convert.ToInt32(Settings.settingsObj.GetValue("ImageRetentionTime"));
-
-                if (!Settings.settingsObj.TryGetValue("ClipboardTemplate", out _))
-                    Settings.settingsObj["ClipboardTemplate"] = "-- by WFInfo (smart OCR with pricecheck)";
-                Settings.ClipboardTemplate = Convert.ToString(Settings.settingsObj.GetValue("ClipboardTemplate"));
-
-                if (!Settings.settingsObj.TryGetValue("SnapitExport", out _))
-                    Settings.settingsObj["SnapitExport"] = false;
-                Settings.SnapitExport = Convert.ToBoolean(Settings.settingsObj.GetValue("SnapitExport"));
-
-                if (!Settings.settingsObj.TryGetValue("Delay", out _))
-                    Settings.settingsObj["Delay"] = 10000;
-                Settings.delay = Convert.ToInt32(Settings.settingsObj.GetValue("Delay"));
-
-                if (!Settings.settingsObj.TryGetValue("Highlight", out _))
-                    Settings.settingsObj["Highlight"] = false;
-                Settings.Highlight = Convert.ToBoolean(Settings.settingsObj.GetValue("Highlight"));
-
-                if (!Settings.settingsObj.TryGetValue("ClipboardVaulted", out _))
-                    Settings.settingsObj["ClipboardVaulted"] = false;
-                Settings.ClipboardVaulted = (bool)Settings.settingsObj.GetValue("ClipboardVaulted");
-
-                Settings.Save();
+                InitializeSettings();
 
                 LowLevelListener.KeyEvent += main.OnKeyAction;
                 LowLevelListener.MouseEvent += main.OnMouseAction;
@@ -154,6 +75,85 @@ namespace WFInfo
             {
                 Main.AddLog("An error occured while loading the main window: " + e.Message);
             }
+        }
+
+        private void InitializeSettings() {
+            if (!Settings.settingsObj.TryGetValue("Display", out _))
+                Settings.settingsObj["Display"] = "Overlay";
+            Settings.isOverlaySelected = Settings.settingsObj.GetValue("Display").ToString() == "Overlay";
+            Settings.isLightSlected = Settings.settingsObj.GetValue("Display").ToString() == "Light";
+
+            if (!Settings.settingsObj.TryGetValue("MainWindowLocation_X", out _))
+                Settings.settingsObj["MainWindowLocation_X"] = 300;
+            if (!Settings.settingsObj.TryGetValue("MainWindowLocation_Y", out _))
+                Settings.settingsObj["MainWindowLocation_Y"] = 300;
+            Settings.mainWindowLocation = new Point(Settings.settingsObj.GetValue("MainWindowLocation_X").ToObject<Int32>(), Settings.settingsObj.GetValue("MainWindowLocation_Y").ToObject<Int32>());
+
+            if (!Settings.settingsObj.TryGetValue("ActivationKey", out _))
+                Settings.settingsObj["ActivationKey"] = "Snapshot";
+            try {
+                Settings.ActivationKey = (Key)Enum.Parse(typeof(Key), Settings.settingsObj.GetValue("ActivationKey").ToString());
+            }
+            catch {
+                try {
+                    Settings.ActivationMouseButton = (MouseButton)Enum.Parse(typeof(MouseButton), Settings.settingsObj.GetValue("ActivationKey").ToString());
+                }
+                catch {
+                    Main.AddLog("Couldn't Parse Activation Key -- Defaulting to PrintScreen");
+                    Settings.settingsObj["ActivationKey"] = "Snapshot";
+                    Settings.ActivationKey = Key.Snapshot;
+                }
+            }
+
+            if (!Settings.settingsObj.TryGetValue("Debug", out _))
+                Settings.settingsObj["Debug"] = false;
+            Settings.debug = (bool)Settings.settingsObj.GetValue("Debug");
+
+            if (!Settings.settingsObj.TryGetValue("Clipboard", out _))
+                Settings.settingsObj["Clipboard"] = false;
+            Settings.clipboard = (bool)Settings.settingsObj.GetValue("Clipboard");
+
+            if (!Settings.settingsObj.TryGetValue("Auto", out _))
+                Settings.settingsObj["Auto"] = false;
+            Settings.auto = (bool)Settings.settingsObj.GetValue("Auto");
+
+            if (!Settings.settingsObj.TryGetValue("DetectScaling", out _))
+                Settings.settingsObj["DetectScaling"] = true;
+            Settings.autoScaling = (bool)Settings.settingsObj.GetValue("DetectScaling");
+
+            if (!Settings.settingsObj.TryGetValue("AutoDelay", out _))
+                Settings.settingsObj["AutoDelay"] = 250L;
+            Settings.autoDelay = (long)Settings.settingsObj.GetValue("AutoDelay");
+
+            if (!Settings.settingsObj.TryGetValue("Scaling", out _))
+                Settings.settingsObj["Scaling"] = 100.0;
+            Settings.scaling = Convert.ToInt32(Settings.settingsObj.GetValue("Scaling"));
+
+            if (!Settings.settingsObj.TryGetValue("ImageRetentionTime", out _))
+                Settings.settingsObj["ImageRetentionTime"] = 12;
+            Settings.imageRetentionTime = Convert.ToInt32(Settings.settingsObj.GetValue("ImageRetentionTime"));
+
+            if (!Settings.settingsObj.TryGetValue("ClipboardTemplate", out _))
+                Settings.settingsObj["ClipboardTemplate"] = "-- by WFInfo (smart OCR with pricecheck)";
+            Settings.ClipboardTemplate = Convert.ToString(Settings.settingsObj.GetValue("ClipboardTemplate"));
+
+            if (!Settings.settingsObj.TryGetValue("SnapitExport", out _))
+                Settings.settingsObj["SnapitExport"] = false;
+            Settings.SnapitExport = Convert.ToBoolean(Settings.settingsObj.GetValue("SnapitExport"));
+
+            if (!Settings.settingsObj.TryGetValue("Delay", out _))
+                Settings.settingsObj["Delay"] = 10000;
+            Settings.delay = Convert.ToInt32(Settings.settingsObj.GetValue("Delay"));
+
+            if (!Settings.settingsObj.TryGetValue("HighlightRewards", out _))
+                Settings.settingsObj["HighlightRewards"] = true;
+            Settings.Highlight = Convert.ToBoolean(Settings.settingsObj.GetValue("HighlightRewards"));
+
+            if (!Settings.settingsObj.TryGetValue("ClipboardVaulted", out _))
+                Settings.settingsObj["ClipboardVaulted"] = false;
+            Settings.ClipboardVaulted = (bool)Settings.settingsObj.GetValue("ClipboardVaulted");
+
+            Settings.Save();
         }
 
         public void OnContentRendered(object sender, EventArgs e)
