4944cd31a8d747d3f1c93fafc86afcd0780e228a
dapal-003 <Dapal003@gmail.com>
2020-06-09 18:23:26 +0200

    Added in first mock up for listing screen

---
+++ WFInfo/CreateListingDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace WFInfo {
+	/// <summary>
+	/// Interaction logic for CreateListing.xaml
+	/// </summary>
+	public partial class CreateListing : Window {
+
+		#region default methods
+		public CreateListing() {
+			InitializeComponent();
+		}
+
+		private void Hide(object sender, RoutedEventArgs e) {
+			Hide();
+		}
+
+		// Allows the draging of the window
+		private new void MouseDown(object sender, MouseButtonEventArgs e) {
+			if (e.ChangedButton == MouseButton.Left)
+				DragMove();
+		}
+		#endregion
+
+	}
+}
