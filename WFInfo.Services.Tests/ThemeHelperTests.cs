a985e40280bc23707c79a7c1ec98256db3ffd0a9
Thomas Boby <th.boby@gmail.com>
2021-11-20 13:58:23 +0000

    Download image

--- WFInfo.Services.Tests/ThemeHelperTests.cs
+++ WFInfo.Services.Tests/ThemeHelperTests.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Drawing;
 using System.Globalization;
+using System.IO;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using WFInfo.Services.OCR;
 using Xunit;
 
@@ -10,14 +12,25 @@ namespace WFInfo.Services.Tests
     public class ThemeHelperTests
     {
         [Fact]
-        public void TestDarkLotus()
+        public async Task TestDarkLotus()
         {
+            var filepath = "https://i.imgur.com/9PYYS9c.png";
             try
             {
                 AppContext.SetSwitch("System.Drawing.EnableUnixSupport", true);
             }
             catch(Exception){}
-            var bitmap = new Bitmap(@"D:\WFinfo\Images\darklotus_part4_720p_50_50\SSCLEAN-193.png");
+
+            Bitmap bitmap;
+            using (var webClient = new System.Net.Http.HttpClient())
+            {
+                await using (Stream stream = await webClient.GetStreamAsync(filepath))
+                {
+                    bitmap = new Bitmap(stream);
+                }
+            }
+ 
+            // var bitmap = new Bitmap(@"D:\WFinfo\Images\darklotus_part4_720p_50_50\SSCLEAN-193.png");
             var theme = ThemeHelpers.GetThemeWeighted(out var closestThresh, 1, s => { }, CultureInfo.CurrentCulture, bitmap);
             Assert.Equal(WFtheme.DARK_LOTUS, theme);
         }
