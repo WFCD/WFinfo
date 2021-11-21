87057a27511b129a0e9147da9aa04cd52b84c3b0
Thomas Boby <th.boby@gmail.com>
2021-11-21 12:17:11 +0000

    x

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using Tesseract;
+using WFInfo.Services.Tests;
+
+Console.WriteLine("Hello, World!");
+var test = new RewardHelperTests();
+await test.Test();
