7c2fa0e252d3771692d3a0e47f8d25d921fb91b4
Thomas Boby <th.boby@gmail.com>
2022-01-22 13:08:35 +0000

    Remove temp project

--- ConsoleApp1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System;
-using Tesseract;
-using WFInfo.Services.Tests;
-
-Console.WriteLine("Hello, World!");
-var test = new RewardHelperTests();
-await test.Test();
