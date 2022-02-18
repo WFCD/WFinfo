5a5c714fa7d5b291af69ba80bc3e42dd5fc603f9
BLG51 <zary1998mail.ru>
2022-02-18 04:38:00 +0300

    corrected shortest item length

--- WFInfo/Ocr.cs
+++ WFInfo/Ocr.cs
@@ -788,9 +788,8 @@ namespace WFInfo
         /// <returns>If part name is close enough to valid to actually process</returns>
         internal static bool PartNameValid (string partName)
         {
-            if ((partName.Length < 13 && _settings.Locale == "en") || (partName.Replace(" ", "").Length < 6 && _settings.Locale == "ko") || (partName.Length < 16 &&_settings.Locale == "ru")) // if part name is smaller than "Bo prime handle" skip current part 
+            if ((partName.Length < 13 && _settings.Locale == "en") || (partName.Replace(" ", "").Length < 6 && _settings.Locale == "ko") || (partName.Length < 13 &&_settings.Locale == "ru")) // if part name is smaller than "Bo prime handle" skip current part 
                 //TODO: Add a min character for other locale here.
-                //im just guessing about ru for now
                 return false;
             return true;
         }
