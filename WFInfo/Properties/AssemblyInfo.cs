b9bb4ea3af1035bc54bdd35786d0727d4f5c187c
Dmitry Romanenko <dimon222@gmail.com>
2022-05-12 23:47:20 +0000

    Bump to 9.5.3

--- WFInfo/Properties/AssemblyInfo.cs
+++ WFInfo/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WFInfo")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("9.5.2.0")]
-[assembly: AssemblyFileVersion("9.5.2.0")]
+[assembly: AssemblyVersion("9.5.3.0")]
+[assembly: AssemblyFileVersion("9.5.3.0")]
