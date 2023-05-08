3069f1d28ba404919d45b1a9089c33eb2a515168
virusek20 <gmoder.virusek20@gmail.com>
2023-05-08 17:03:00 +0200

    Remove more useless capture helper methods

--- WFInfo/Services/Screenshot/Composition.WindowsRuntimeHelpers/CaptureHelper.cs
+++ WFInfo/Services/Screenshot/Composition.WindowsRuntimeHelpers/CaptureHelper.cs
@@ -34,16 +34,6 @@ namespace Composition.WindowsRuntimeHelpers
         static readonly Guid GraphicsCaptureItemGuid = new Guid("79C3F95B-31F7-4EC2-A464-632EF5D30760");
 
         [ComImport]
-        [Guid("3E68D4BD-7135-4D10-8018-9FB6D9F33FA1")]
-        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        [ComVisible(true)]
-        interface IInitializeWithWindow
-        {
-            void Initialize(
-                IntPtr hwnd);
-        }
-
-        [ComImport]
         [Guid("3628E81B-3CAC-4C60-B7F4-23CE0E0C3356")]
         [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
         [ComVisible(true)]
@@ -56,12 +46,6 @@ namespace Composition.WindowsRuntimeHelpers
             IntPtr CreateForMonitor(
                 [In] IntPtr monitor,
                 [In] ref Guid iid);
-        }
-
-        public static void SetWindow(this GraphicsCapturePicker picker, IntPtr hwnd)
-        {
-            var interop = (IInitializeWithWindow)(object)picker;
-            interop.Initialize(hwnd);
         }
 
         public static GraphicsCaptureItem CreateItemForWindow(IntPtr hwnd)
