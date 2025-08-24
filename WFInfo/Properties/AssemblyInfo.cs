7119230fae497670c2848e5efb0ef6466e26e164
Dmitry Romanenko <Dmitry@Romanenko.in>
2025-08-24 17:59:17 -0400

    Bump version of WFM to prepare for release

--- WFInfo/Properties/AssemblyInfo.cs
+++ WFInfo/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WFInfo")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("9.6.5.0")]
-[assembly: AssemblyFileVersion("9.6.5.0")]
+[assembly: AssemblyVersion("9.6.6.0")]
+[assembly: AssemblyFileVersion("9.6.6.0")]
